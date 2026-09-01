1ee294eaf7f8832eaf3487da5d2fe336933086bf
412-13-kantapat <54313@yupparaj.ac.th>
2026-09-01 10:49:51 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -8,6 +8,9 @@ if "ans1_val" not in st.session_state:
     st.session_state.ans1_val = ""
 if "ans2_val" not in st.session_state:
     st.session_state.ans2_val = ""
+if "ans3_val" not in st.session_state:
+    st.session_state.ans3_val = ""
+
 
 
 # 📌 ฟังก์ชันเคลียร์ค่าเมื่อกดปุ่มเริ่มใหม่
